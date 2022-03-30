1938810414e5e8760f9b468d20a3c675493d6fe1
Yatao Bian <yatao.bian@gmail.com>
2022-03-30 16:28:52 +0800

    small fix

--- configs/curators/sbap_core_ic50_assay.py
+++ configs/curators/sbap_core_ic50_assay.py
@@ -1,6 +1,6 @@
 _base_ = ['../_base_/curators/sbap_defaults.py',
           '../_base_/curators/noises/core.py',
-          '../_base_/curators/domains/size.py']
+          '../_base_/curators/domains/assay.py']
 
 path = dict(task=dict(subset="sbap_core_ic50_assay"))
 
