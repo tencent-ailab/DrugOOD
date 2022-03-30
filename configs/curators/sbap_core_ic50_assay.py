e5deb16eee0890398a2ab6d4395046f910e4a788
Yatao Bian <yatao.bian@gmail.com>
2022-03-30 16:19:56 +0800

    fix data subset name

--- configs/curators/sbap_core_ic50_assay.py
+++ configs/curators/sbap_core_ic50_assay.py
@@ -2,6 +2,6 @@ _base_ = ['../_base_/curators/sbap_defaults.py',
           '../_base_/curators/noises/core.py',
           '../_base_/curators/domains/size.py']
 
-path = dict(task=dict(subset="sbap_core_ic50_size"))
+path = dict(task=dict(subset="sbap_core_ic50_assay"))
 
 noise_filter = dict(assay=dict(measurement_type=['IC50'], assay_target_type=["SINGLE PROTEIN"]))
